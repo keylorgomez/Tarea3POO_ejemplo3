f75b51a41c681c701a136591dd2e846b35b5bef8
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-11-27 21:10:29 -0600

    Se establecen los metodos en la clase persona y con cual de estos va a iniciar al correr el programa

--- src/ejemplo3/ListoTrabajar.java
+++ src/ejemplo3/ListoTrabajar.java
@@ -1,4 +1,31 @@
 package ejemplo3;
 
-public class ListoTrabajar {
+public class ListoTrabajar implements State{
+    private Persona persona;
+    @Override
+    public void trabajar() {
+        System.out.println("Listo, trabajemos!");
+        persona.cambiarState(new Hambriento());
+    }
+
+    @Override
+    public void comer() {
+        System.out.println("No quiero comer");
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println("No quiero dormir");
+    }
+
+    @Override
+    public void comoEsta() {
+        System.out.println("Estoy listo para trabajar");
+    }
+
+    @Override
+    public void cambiarPersona(Persona persona) {
+        this.persona=persona;
+
+    }
 }
